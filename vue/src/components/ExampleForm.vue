954dc9c5b2da6cc6916dc0cb49379fbb9a68e8da
Jacob Crider <jpc214@gmail.com>
2021-12-08 12:34:16 -0500

    add examples and examples cards completed

--- vue/src/components/ExampleForm.vue
+++ vue/src/components/ExampleForm.vue
@@ -1,68 +1,128 @@
 <template>
   <div>
-      <form>
-          <label for="title">Title</label>
-          <input type="text" id="title" v-model="example.title">
+    <form>
+      <div>
+        <label for="title">Title</label>
+        <input type="text" id="title" v-model="example.title" />
+      </div>
 
-          <label for="snippet">Snippet</label>
-          <textarea id="snippet" v-model="example.snippet"></textarea>
+      <div>
+        <label for="snippet">Snippet</label>
+        <textarea id="snippet" v-model="example.snippet"></textarea>
+      </div>
 
-          <label for="languageType">Language</label>
-          <select v-model="example.languageType">
-            <option value="">Show All</option>
-            <option >JavaScript</option>
-            <option >Java</option>
-            <option >SQL</option>
-          </select>
-          
-          <p>Tags</p>
-          <input v-model="example.tagList" type="checkbox" id="Loops" name="Loops" v-bind:value="{name: 'Loops'}">
-            <label for="Loops">Loops</label>
-          <input v-model="example.tagList" type="checkbox" id="Branches" name="Branches" v-bind:value="{name: 'Branches'}">
-            <label for="Branches">Branches</label>
-          <input v-model="example.tagList" type="checkbox" id="Arrays" name="Arrays" v-bind:value="{name: 'Arrays'}">
-            <label for="Arrays">Arrays</label>
-          <input v-model="example.tagList" type="checkbox" id="Other" name="Other" v-bind:value="{name: 'Other'}">
-            <label for="Other">Other</label>      
+      <div>
+        <label for="languageType">Language</label>
+        <select v-model="example.languageType">
+          <option value="">Show All</option>
+          <option>JavaScript</option>
+          <option>Java</option>
+          <option>SQL</option>
+        </select>
+      </div>
 
-          <button @click.prevent="addExample">Submit</button>
-      </form>
+      <div>
+        <span>Tags:</span>
+
+        <label for="Loops">Loops</label>
+        <input
+          v-model="example.tagList"
+          type="checkbox"
+          id="Loops"
+          name="Loops"
+          v-bind:value="{ name: 'Loops' }"
+        />
+
+        <label for="Branches">Branches</label>
+         <input
+          v-model="example.tagList"
+          type="checkbox"
+          id="Branches"
+          name="Branches"
+          v-bind:value="{ name: 'Branches' }"
+        />
+
+        <label for="Arrays">Arrays</label>
+        <input
+          v-model="example.tagList"
+          type="checkbox"
+          id="Arrays"
+          name="Arrays"
+          v-bind:value="{ name: 'Arrays' }"
+        />
+        
+    
+        <label for="Other">Other</label>
+        <input
+          v-model="example.tagList"
+          type="checkbox"
+          id="Other"
+          name="Other"
+          v-bind:value="{ name: 'Other' }"
+        />
+        
+      </div>
+
+      <button @click.prevent="addExample">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-import exampleService from '../services/ExampleService';
+import exampleService from "../services/ExampleService";
 
 export default {
-    data() {
-        return {
-            example: {
-                title: '',
-                snippet: '',
-                languageType: '',
-                tagList: []
-            }
-        }
-    }, 
-    methods: {
-        addExample() {
-            exampleService.addExample(this.example).then(response => {
-                if (response.status == 201) {
-                    this.$router.push('/');
-                }
-            }).catch(error => {
-                if (error.response.status == 400) {
-                    // TODO: Route user to error page
-                    console.error(error);
-                } else {
-                    console.error(error);
-                }
-            });
-        },
-    }
-}
+  data() {
+    return {
+      example: {
+        title: "",
+        snippet: "",
+        languageType: "",
+        tagList: [],
+      },
+    };
+  },
+  methods: {
+    addExample() {
+      exampleService
+        .addExample(this.example)
+        .then((response) => {
+          if (response.status == 201) {
+            this.$router.push("/");
+          }
+        })
+        .catch((error) => {
+          if (error.response.status == 400) {
+            // TODO: Route user to error page
+            console.error(error);
+          } else {
+            console.error(error);
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style>
+    label {
+        display: inline-block;
+        vertical-align: top;
+        width: 10ch;
+        text-align: right;
+        padding-right: 4px;
+    }
 
+    textarea {
+        height: 4rem;
+        width: 60ch;
+    }
+
+    span {
+        display: inline-block;
+        vertical-align: top;
+        width: 10ch;
+        text-align: right;
+        padding-right: 4px;
+    }
 </style>
