9e4d66346e3565e1f467022c8378237780e18339
Jacob Crider <jpc214@gmail.com>
2021-12-09 12:01:11 -0500

    pushing language.vue and languagelist.vue

---
+++ vue/src/components/Language.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <label for="type"></label>
+      <input type="text">
+
+      <label for="active"></label>
+      <input type="checkbox">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Language',
+    props: ['language']
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/LanguageList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <language v-for="currentLanguage in languages" v-bind:language="currentLanguage" v-bind:key="currentLanguage.id"/>
+  </div>
+</template>
+
+<script>
+import Language from './Language.vue'
+import languageService from '../services/LanguageService'
+
+export default {
+    name: 'LanguageList',
+    components: {
+        Language
+    },
+    data() {
+        return {
+            languages: []
+        }
+    },
+    created() {
+        languageService.getAllLanguages.
+    }
+}
+</script>
+
+<style>
+
+</style>
