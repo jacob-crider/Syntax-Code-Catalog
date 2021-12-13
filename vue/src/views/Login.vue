6c4e51b9a9c165865d6dd8d9ee7cd02f2ce6eb6d
Jacob Crider <jpc214@gmail.com>
2021-12-13 16:13:33 -0500

    styling for login

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -2,45 +2,51 @@
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
       <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
+      <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+        Invalid username and password!
+      </div>
       <div
         class="alert alert-success"
         role="alert"
         v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <div class="form-inputs">
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
+      >
+        Thank you for registering, please sign in.
       </div>
-      <div class="form-inputs">
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      </div>
-      
-      <div class="register-link">
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      </div>
-      <div class="submit-form">
-      <button type="submit">Sign in</button>
+      <div>
+        <div class="login">
+          <div class="form-inputs">
+            <label for="username" class="sr-only">Username</label>
+            <input
+              type="text"
+              id="username"
+              class="form-control"
+              placeholder="Username"
+              v-model="user.username"
+              required
+              autofocus
+            />
+          </div>
+          <div class="form-inputs">
+            <label for="password" class="sr-only">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              placeholder="Password"
+              v-model="user.password"
+              required
+            />
+          </div>
+
+          <div class="register-link">
+            <router-link :to="{ name: 'register' }"
+              >Need an account?</router-link
+            >
+          </div>
+          <div class="submit-form">
+            <button type="submit">Sign in</button>
+          </div>
+        </div>
       </div>
     </form>
   </div>
@@ -56,30 +62,48 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style>
+h1 {
+  text-align: center;
+}
+
+div.login {
+  width: 40%;
+
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+
+div.form-inputs {
+}
+</style>
