0407a059d38cb045c63477f1a81a650603312067
Jacob Crider <jpc214@gmail.com>
2021-12-14 17:09:18 -0500

    mixed in styling and comment functionality

--- vue/src/components/CommentList.vue
+++ vue/src/components/CommentList.vue
@@ -1,47 +1,57 @@
 <template>
   <div>
-      <input type="text" v-model="comment.description">
-      <button @click.prevent="addComment">Add Comment</button>
-      <comment v-for="currentComment in comments" v-bind:comment="currentComment" v-bind:key="currentComment.id"/>
+    <button @click.prevent="addComment">Add Comment</button>
+    <input type="text" v-model="comment.description" />
+    <div v-if="comments !== []">
+      <comment
+        v-for="currentComment in comments"
+        v-bind:comment="currentComment"
+        v-bind:key="currentComment.id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import Comment from './Comment.vue'
-import exampleService from '../services/ExampleService'
+import Comment from "./Comment.vue";
+import exampleService from "../services/ExampleService";
 
 export default {
-    name: 'CommentList',
-    components: {
-        Comment
+  name: "CommentList",
+  components: {
+    Comment,
+  },
+  props: ["example"],
+  data() {
+    return {
+      comments: [],
+      comment: {
+        exampleId: this.example.exampleID,
+        description: "",
+      },
+    };
+  },
+  created() {
+    this.comments = this.example.comments.slice();
+  },
+  methods: {
+    addComment() {
+      exampleService
+        .addComment(this.comment)
+        .then((response) => {
+          if (response.status === 200) {
+            console.log("Success!");
+            this.comment.description = '';
+            this.$router.go(this.$router.currentRoute);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    props: ['example'],
-    data() {
-        return {
-            comments: [],
-            comment: {
-            exampleId: this.example.exampleId,
-            description: ''
-            }
-        }
-    },
-    created() {
-        this.comments = this.example.comments;
-    },
-    methods: {
-        addComment() {
-            exampleService.addComment(this.comment).then(response => {
-                if (response.status === 200) {
-                    console.log("Success!");
-                }
-            }).catch(error => {
-                console.error(error);
-            })
-        } 
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
